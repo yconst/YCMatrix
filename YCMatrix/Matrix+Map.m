8fa4f81861d0950b6de84f59f3af722d47cee556
Ioannis Chatzikonstantinou <contact@yconst.com>
2015-07-05 16:34:10 +0300

    Implemented column-wise mapping.

--- YCMatrix/Matrix+Map.m
+++ YCMatrix/Matrix+Map.m
@@ -141,4 +141,23 @@
     return transform;
 }
 
+
+- (Matrix *)matrixByColumnWiseMapUsing:(Matrix *)transform
+{
+    // TODO: Reimplement to improve performance (remove extra transposition)
+    return [[self matrixByTransposing] matrixByRowWiseMapUsing:transform];
+}
+
+- (Matrix *)columnWiseMapToDomain:(YCDomain)domain basis:(MapBasis)basis
+{
+    // TODO: Reimplement to improve performance (remove extra transposition)
+    return [[self matrixByTransposing] rowWiseMapToDomain:domain basis:basis];
+}
+
+- (Matrix *)columnWiseInverseMapFromDomain:(YCDomain)domain basis:(MapBasis)basis
+{
+    // TODO: Reimplement to improve performance (remove extra transposition)
+    return [[self matrixByTransposing] rowWiseInverseMapFromDomain:domain basis:basis];
+}
+
 @end
